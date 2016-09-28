b9b6c4b5ae52345698bc08ae8bb0c23c5167364f
elvis1020 <elvis.shock@gmail.com>
2016-09-28 11:19:51 +0000

    xplotlayout

---
+++ gui/XPlotLayout.py
@@ -0,0 +1,15 @@
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+
+class XPlotLayout(QMainWindow):
+    def __init__(self, *args):
+        QMainWindow.__init__(self, *args)
+
+
+
+app = QApplication([])
+form = MyWindow()
+form.show()
+app.exec_()
+
